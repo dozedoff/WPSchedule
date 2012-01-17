a2a83c77f5ff037abc59923804a04d902fa93739
dozedoff <dozedoffagain@gmail.com>
2012-01-17 19:59:23 +0100

    test setup done

--- WPSchedule/test/dozedoff/WPSchedule/ScheduleTest.java
+++ WPSchedule/test/dozedoff/WPSchedule/ScheduleTest.java
@@ -1,51 +1,73 @@
 package dozedoff.WPSchedule;
 
-import junit.framework.TestCase;
+import org.junit.*;
 
-public class ScheduleTest extends TestCase {
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
-	public void testAddGroup() {
-		fail("Not yet implemented");
+
+public class ScheduleTest {
+	Schedule schedule;
+	ImageGroup mockImageGroup;
+	
+	@Before
+	public void setUp(){
+		schedule = new Schedule();
+		mockImageGroup = mock(ImageGroup.class);
 	}
 
+	@Test
+	public void testAddGroup() {
+		
+	}
+
+	@Test
 	public void testIsActive() {
 		fail("Not yet implemented");
 	}
 
+	@Test
 	public void testIsEnabled() {
 		fail("Not yet implemented");
 	}
 
+	@Test
 	public void testSetEnabled() {
 		fail("Not yet implemented");
 	}
 
+	@Test
 	public void testIsRandomOrder() {
 		fail("Not yet implemented");
 	}
 
+	@Test
 	public void testSetRandomOrder() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
 	public void testGetStartTime() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
 	public void testGetEndTime() {
 		fail("Not yet implemented");
 	}
 
+	@Test
 	public void testSetTimeWindow() {
 		fail("Not yet implemented");
 	}
 
+	@Test
 	public void testGetImageGroups() {
 		fail("Not yet implemented");
 	}
 
+	@Test
 	public void testAddImageGroup() {
 		fail("Not yet implemented");
 	}
-
 }
