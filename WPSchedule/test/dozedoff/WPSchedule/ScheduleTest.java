6788dcda3e5b863c5b5eaa1e73895107bbeb8f2e
dozedoff <dozedoffagain@gmail.com>
2012-01-17 19:00:31 +0100

    added ScheduleTest

---
+++ WPSchedule/test/dozedoff/WPSchedule/ScheduleTest.java
@@ -0,0 +1,51 @@
+package dozedoff.WPSchedule;
+
+import junit.framework.TestCase;
+
+public class ScheduleTest extends TestCase {
+
+	public void testAddGroup() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsActive() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsEnabled() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetEnabled() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsRandomOrder() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetRandomOrder() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetStartTime() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetEndTime() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetTimeWindow() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetImageGroups() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddImageGroup() {
+		fail("Not yet implemented");
+	}
+
+}
