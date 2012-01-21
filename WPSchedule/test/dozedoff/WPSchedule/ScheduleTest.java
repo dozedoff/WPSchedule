531009ec340b9282aec9ac9a1ef84ef6154a6802
dozedoff <dozedoffagain@gmail.com>
2012-01-22 00:22:39 +0100

    more tests

--- WPSchedule/test/dozedoff/WPSchedule/ScheduleTest.java
+++ WPSchedule/test/dozedoff/WPSchedule/ScheduleTest.java
@@ -1,9 +1,12 @@
 package dozedoff.WPSchedule;
 
+import java.util.Date;
+
 import org.junit.*;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.matchers.JUnitMatchers.*;
 
 public class ScheduleTest {
 	Schedule schedule;
@@ -18,12 +21,32 @@ public class ScheduleTest {
 		assertThat(schedule.addImageGroup(new ImageGroup("New Group")), is(true));
 		assertThat(schedule.addImageGroup(new ImageGroup("New Group")), is(false));
 		assertThat(schedule.addImageGroup(new ImageGroup("group one")), is(true));
+		
+		assertThat(schedule.getImageGroups(), hasItem(new ImageGroup("New Group")));
+		assertThat(schedule.getImageGroups(), hasItem(new ImageGroup("group one")));
 	}
 
 	@Test
 	public void testIsActive() {
-		//TODO SO article about dates
-		fail("Not yet implemented");
+		// using diffrent years to make sure only time is compared
+		Date midnight = new Date(15,0,0,0,0);
+		Date sixAm = new Date(23,0,0,6,0);
+		Date elevenAm = new Date(54,0,0,11,0);
+		Date twoPm = new Date(2, 0, 0, 14, 0);
+		Date ninePm = new Date(12, 0, 0, 21, 0);
+		
+		assertThat(schedule.isActive(midnight), is(false));
+		assertThat(schedule.isActive(ninePm), is(true));
+		
+		schedule.setTimeWindow(new Date(0,0,0,14,0), new Date(0, 0, 0, 13, 0));
+		
+		assertThat(schedule.isActive(midnight), is(false));
+		assertThat(schedule.isActive(sixAm ), is(false));
+		assertThat(schedule.isActive(elevenAm), is(true));
+		assertThat(schedule.isActive(twoPm), is(true));
+		assertThat(schedule.isActive(ninePm), is(false));
+		
+		//TODO add test for incorrect time window definition
 	}
 
 
