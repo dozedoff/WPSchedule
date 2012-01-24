3d35eec31db608a1c5d4e0e534884808c1d25a4d
dozedoff <dozedoffagain@gmail.com>
2012-01-24 16:56:47 +0100

    more tests

--- WPSchedule/test/dozedoff/WPSchedule/ImageGroupTest.java
+++ WPSchedule/test/dozedoff/WPSchedule/ImageGroupTest.java
@@ -1,24 +1,65 @@
 package dozedoff.WPSchedule;
 
 import static org.junit.Assert.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.matchers.JUnitMatchers.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
 public class ImageGroupTest {
-
+	ImageGroup ig;
+	
+	ArrayList<File> testf;
+	ArrayList<File> testd;
+	ArrayList<File> testfsub;
+	ArrayList<File> all;
+	
 	@Before
 	public void setUp() throws Exception {
+		ig = new ImageGroup("test");
+		
+		testf = new ArrayList<File>();
+		testd = new ArrayList<File>();
+		testfsub = new ArrayList<File>();
+		all = new ArrayList<File>();
+		
+		createFiles();
 	}
 
 	@Test
-	public void testValidate() {
+	public void testValidate() throws IOException {
+		verifyFiles();
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAddFile(){
+		verifyFiles();
+		
+		for(File file : testf){
+			ig.addFile(file);
+		}
+		
+		ig.addFile(testd.get(0));
+		
+		ig.addFile(null);
+		
+		assertThat(ig.getImages().size(), is(2));
+		assertThat(ig.getImages(), hasItem(testf.get(0)));
+		assertThat(ig.getImages(), hasItem(testf.get(1)));
 	}
 
 	@Test
 	public void testEqualsObject() {
-		fail("Not yet implemented");
+		assertThat(ig.equals(new ImageGroup("test")), is(true));
+		assertThat(ig.equals(new ImageGroup(null)), is(false));
+		assertThat(ig.equals(new ImageGroup("TEST")), is(false));
+		assertThat(ig.equals(new int[3]), is(false));
 	}
 
 	@Test
@@ -30,5 +71,43 @@ public class ImageGroupTest {
 	public void testGetSequentialImage() {
 		fail("Not yet implemented");
 	}
-
+	
+	private void createFiles() throws IOException{
+		File tmpDir = Files.createTempDirectory("ImageGroupTest").toFile();
+		
+		testf.add(new File(tmpDir,"file1.jpg"));
+		testf.add(new File(tmpDir,"file2.png"));
+		testf.add(new File(tmpDir,"file3.bmp"));
+		testf.add(new File(tmpDir,"file4.txt"));
+		
+		testd.add(new File(tmpDir,"subDir1"));
+		testd.add(new File(tmpDir,"subDir2"));
+		
+		File sub2 = testd.get(1);
+		testfsub.add(new File(sub2,"test.jpg"));
+		testfsub.add(new File(sub2, "test.png"));
+		testfsub.add(new File(sub2, "test.txt"));
+		
+		for(File dir : testd){
+			dir.mkdirs();
+		}
+		
+		for(File file : testf){
+			file.createNewFile();
+		}
+		
+		for(File file : testfsub){
+			file.createNewFile();
+		}
+		
+		all.addAll(testf);
+		all.addAll(testd);
+		all.addAll(testfsub);
+	}
+	
+	private void verifyFiles(){
+		for(File file : all){
+			assertTrue("File "+file.toString()+" is missing",file.exists()); // make sure all files have been created
+		}
+	}
 }
