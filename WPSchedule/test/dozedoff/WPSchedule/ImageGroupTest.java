3f93dfd70429f494c6a4cbfef1f0317f3db4a47a
dozedoff <dozedoffagain@gmail.com>
2012-01-24 07:18:28 +0100

    added ImageGroupTest

---
+++ WPSchedule/test/dozedoff/WPSchedule/ImageGroupTest.java
@@ -0,0 +1,34 @@
+package dozedoff.WPSchedule;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImageGroupTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testValidate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRandomImage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSequentialImage() {
+		fail("Not yet implemented");
+	}
+
+}
