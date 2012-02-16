b3b470e6a7cf11bbfba0cff9d27974e859285c8a
dozedoff <dozedoffagain@gmail.com>
2012-02-16 23:24:23 +0100

    added AllTests test suit

---
+++ WPSchedule/test/dozedoff/WPSchedule/AllTests.java
@@ -0,0 +1,14 @@
+package dozedoff.WPSchedule;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+		ImageGroupTest.class, 
+		ScheduleTest.class
+		})
+public class AllTests {
+
+}
